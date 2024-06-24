349b9052c59a916a14468476bd217eca8873c17d
voke sokoh <vokes9810@gmail.com>
2024-06-24 06:17:22 +0100

    the z-index is having an effect here,the top-card image is being tucked inthe main-card.  let me work with this branch

--- src/components/dashboards/job-timeframe/JobTimeframe.jsx
+++ src/components/dashboards/job-timeframe/JobTimeframe.jsx
@@ -1,3 +1,53 @@
+// import classes from "./JobTimeframe.module.css";
+// import { jobTimeFrames } from "../../home/TimeFrames";
+// import Card from "../../ui/card/Card";
+
+// const JobTimeFrame = (props) => {
+//   const jobTimeFrameArray = jobTimeFrames;
+//   console.log(jobTimeFrameArray);
+
+//   return (
+//     <div className={classes["timeFrame-container"]}>
+//       <Card className={classes["top-card"]}>
+//         <img src="" alt="" />
+//       </Card>
+
+//       <Card className={classes["main-card"]}>
+//         {jobTimeFrameArray.map((jtf) => {
+//           return (
+//             <div key={jtf.title} className={classes["frame"]}>
+//               <ul>
+//                 <div className={classes["first-row"]}>
+//                   <li>
+//                     <h3>
+//                       {jtf.title.charAt(0).toUpperCase() + jtf.title.slice(1)}
+//                     </h3>
+//                   </li>
+//                   <p className={classes.ellypsis}>...</p>
+//                 </div>
+
+//                 <div className={classes["second-row"]}>
+//                   <h2>{jtf.timeframes.daily.current}</h2>
+//                   <h4>Completed</h4>
+//                 </div>
+
+//                 <div className={classes["third-row"]}>
+//                   <small>{`Last Week:${jtf.timeframes.daily.previous} Completed`}</small>
+//                 </div>
+//               </ul>
+//             </div>
+//           );
+//         })}
+//       </Card>
+//     </div>
+//   );
+// };
+
+// export default JobTimeFrame;
+
+
+
+
 import classes from "./JobTimeframe.module.css";
 import { jobTimeFrames } from "../../home/TimeFrames";
 import Card from "../../ui/card/Card";
@@ -9,39 +59,37 @@ const JobTimeFrame = (props) => {
   return (
     <div className={classes["timeFrame-container"]}>
       <Card className={classes["top-card"]}>
-        <img src="" alt="" />
-      </Card>
+        <img src={props.jobImage} alt="" />
+        <Card className={classes["main-card"]}>
+          {jobTimeFrameArray.map((jtf) => {
+            return (
+              <div key={jtf.title} className={classes["frame"]}>
+                <ul>
+                  <div className={classes["first-row"]}>
+                    <li>
+                      <h3>
+                        {jtf.title.charAt(0).toUpperCase() + jtf.title.slice(1)}
+                      </h3>
+                    </li>
+                    <p className={classes.ellypsis}>...</p>
+                  </div>
 
-      <Card className={classes["main-card"]}>
-        {jobTimeFrameArray.map((jtf) => {
-          return (
-            <div key={jtf.title} className={classes["frame"]}>
-              <ul>
-                <div className={classes["first-row"]}>
-                  <li>
-                    <h3>
-                      {jtf.title.charAt(0).toUpperCase() + jtf.title.slice(1)}
-                    </h3>
-                  </li>
-                  <p className={classes.ellypsis}>...</p>
-                </div>
+                  <div className={classes["second-row"]}>
+                    <h2>{jtf.timeframes.daily.current}</h2>
+                    <h4>Completed</h4>
+                  </div>
 
-                <div className={classes["second-row"]}>
-                  <h2>{jtf.timeframes.daily.current}</h2>
-                  <h4>Completed</h4>
-                </div>
-
-                <div className={classes["third-row"]}>
-                  <small>{`Last Week:${jtf.timeframes.daily.previous} Completed`}</small>
-                </div>
-              </ul>
-            </div>
-          );
-        })}
+                  <div className={classes["third-row"]}>
+                    <small>{`Last Week:${jtf.timeframes.daily.previous} Completed`}</small>
+                  </div>
+                </ul>
+              </div>
+            );
+          })}
+        </Card>
       </Card>
     </div>
   );
 };
 
 export default JobTimeFrame;
-
